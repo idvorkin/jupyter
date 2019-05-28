bb82b3a007d82a615180de68a1b4e08f7f763e84
Igor Dvorkin <idvorkin@gmail.com>
2019-05-27 17:01:09 -0700

    Lots of cleanup and refactoring.

--- PlayNLP.py
+++ PlayNLP.py
@@ -26,6 +26,8 @@ from pandas import DataFrame
 import matplotlib as mpl
 """
 
+from typing import List, Tuple
+from dataclasses import dataclass
 import pandas as pd
 
 import matplotlib.pyplot as plt
@@ -41,16 +43,111 @@ from nltk.corpus import stopwords
 
 # get scapy and corpus
 import spacy
+import time
+from functools import lru_cache
+
+
+# %%
+# This function is in the first block so you don't
+# recreate it willy nilly, as it includes a cache.
 
 nltk.download("stopwords")
-english_stop_words = set(stopwords.words("english"))
+
+# Remove domain words that don't help analysis.
+# Should be factored out
+domain_stop_words = set(
+    """
+    yes yup Affirmations get that's
+    Deliberate Disciplined Daily
+    Know Essential Provide Context
+    First Understand Appreciate
+    """.lower().split()
+)
 
 
-# nlp = spacy.load("en_core_web_lg")  # python -m spacy download en_core_web_sm
-spacy.prefer_gpu()
+@lru_cache(maxsize=4)
+def get_nlp_model(model: str):
+    start_time = time.time()
+    print(f"Loading Model {model}")
+    nlp = spacy.load(model)  # python -m spacy download en_core_web_lg
+    spacy.prefer_gpu()  # This will be cool if/when it happens.
+    duration = time.time() - start_time
+    print(f"Took: {int(duration)}")
+    return nlp
 
-# %% [markdown]
-# ### Setup matplot lib to be wider
+
+# Load corpus of my daily ramblings
+@dataclass(frozen=True)
+class Corpus:
+    path: str
+    all_content: str
+    initial_words: List[str]
+    words: List[str]
+
+    def __hash__(self):
+        return self.path.__hash__()
+
+
+@lru_cache(maxsize=10)
+def LoadCorpus(corpus_path: str) -> Corpus:
+
+    # Hym consider memoizing this asweel..
+    english_stop_words = set(stopwords.words("english"))
+    all_stop_words = domain_stop_words | english_stop_words
+
+    corpus_path_expanded = os.path.expanduser(corpus_path)
+    corpus_files = glob.glob(corpus_path_expanded)
+
+    """
+    ######################################################
+    # Performance side-bar.
+    ######################################################
+
+    A] Below code results in all strings Loaded into memory for temporary,  then merged into a second string.
+    aka Memory = O(2*file_conent) and CPU O(2*file_content)
+
+    B] An alternative is to do += on a string results in a new memory allocation and copy.
+    aka Memory = O(file_content) , CPU O(files*file_content)
+
+    However, this stuff needs to be measured, as it's also a funtion of GC. Not in the GC versions there is no change in CPU
+    Eg.
+
+    For A] if GC happens after every "join", then were down to O(file_content).
+    For B] if no GC, then it's still O(2*file_content)
+    """
+
+    # Make single string from all the file contents.
+    list_file_content = [Path(file_name).read_text() for file_name in corpus_files]
+    all_file_content = " ".join(list_file_content)
+
+    # Clean out some punctuation (although does that mess up stemming later??)
+    initial_words = all_file_content.replace(",", " ").replace(".", " ").split()
+
+    words = [word for word in initial_words if word.lower() not in all_stop_words]
+    return Corpus(
+        path=corpus_path,
+        all_content=all_file_content,
+        initial_words=initial_words,
+        words=words,
+    )
+
+
+@lru_cache(maxsize=10)
+def DocForCorpus(nlp, corpus: Corpus):
+    print(
+        f"initial words {len(corpus.initial_words)} remaining words {len(corpus.words)}"
+    )
+    print(f"Building corpus from {corpus.path} of len:{len(corpus.all_content)} ")
+    start_time = time.time()
+    # We use all_file_content not initial_words because we want to keep punctuation.
+    doc_all = nlp(corpus.all_content)
+    duration = time.time() - start_time
+
+    # Remove domain specific stop words.
+    doc = [token for token in doc_all if token.text.lower() not in domain_stop_words]
+    print(f"Took: {int(duration)}")
+    return doc
+
 
 # %%
 # make the plot wider
@@ -61,50 +158,11 @@ matplotlib.rc("figure", figsize=(2 * height_in_inches, height_in_inches))
 # ### Load corpus from my journal
 
 # %%
+corpus_path = "~/gits/igor2/750words/2018*md"
+# corpus_path = "/mnt/c/Users/idvor/OneDrive/backup/Diary/*txt"
 
-# Load corpus of my daily ramblings
-corpus_path = os.path.expanduser("~/gits/igor2/750words/*md")
-# corpus_path = os.path.expanduser("/mnt/c/Users/idvor/OneDrive/backup/Diary/*txt")
-corpus_files = glob.glob(corpus_path)
-
-"""
-######################################################
-# Performance side-bar.
-######################################################
-
-A] Below code results in all strings Loaded into memory for temporary,  then merged into a second string.
-aka Memory = O(2*file_conent) and CPU O(2*file_content)
-
-B] An alternative is to do += on a string results in a new memory allocation and copy.
-aka Memory = O(file_content) , CPU O(files*file_content)
-
-However, this stuff needs to be measured, as it's also a funtion of GC. Not in the GC versions there is no change in CPU
-Eg.
-
-For A] if GC happens after every "join", then were down to O(file_content).
-For B] if no GC, then it's still O(2*file_content)
-"""
-
-# Make single string from all the file contents.
-list_file_content = [Path(file_name).read_text() for file_name in corpus_files]
-all_file_content = " ".join(list_file_content)
-
-# Clean out some punctuation (although does that mess up stemming later??)
-initial_words = all_file_content.replace(",", " ").replace(".", " ").split()
-
-# Remove domain words that don't help analysis.
-domain_stop_words = set(
-    """
-    yes yup Disciplined Daily Know Affirmations Deliberate Essential get that's -
-    Deliberate Disciplined Daily
-    Know Essential Provide Context
-    First Understand Appreciate
-    """.lower().split()
-)
-
-all_stop_words = domain_stop_words | english_stop_words
-words = [word for word in initial_words if word.lower() not in all_stop_words]
-print(f"initial words {len(initial_words)} remaining words {len(words)}")
+corpus = LoadCorpus(corpus_path)
+print(f"initial words {len(corpus.initial_words)} remaining words {len(corpus.words)}")
 
 
 # %%
@@ -124,7 +182,7 @@ def GraphWordDistribution(words, title="", skip=0, length=50, includeCDF=True) -
         cdf = pdf.cumsum()
         return (pdf, cdf)
 
-    def PlotOnAxis(series, ax, label, color):
+    def PlotOnAxis(series, ax, label: str, color: str):
         # RANT: Why is MPL so confusing? The OO interface vs the stateful interface, GRAH!!
         # The random non-obvious calls.
         # GRAH!!!
@@ -159,12 +217,12 @@ def GraphWordDistribution(words, title="", skip=0, length=50, includeCDF=True) -
     PlotOnAxis(cdf[skip : skip + length], ax_cdf, label="CDF*100", color=color_cdf)
 
 
-GraphWordDistribution(words, title="Normalized Word Distribution")
+GraphWordDistribution(corpus.words, title="Normalized Word Distribution")
 
 # %%
 skip = 10
 GraphWordDistribution(
-    words, skip=skip, length=75, title=f"Distribution without top {skip} words"
+    corpus.words, skip=skip, length=75, title=f"Distribution without top {skip} words"
 )
 
 # %%
@@ -177,46 +235,48 @@ GraphWordDistribution(
 # # Play with POS tagging and lemmatisation
 
 # %%
-# Load data model:
-nlp = spacy.load("en_core_web_lg")  # python -m spacy download en_core_web_lg
-nlp.max_length = 20 * 1000 * 1000
+def GraphPoSForDoc(pos, doc):
+    interesting_pos = pos
+    interesting_pos_set = set(interesting_pos.split())
+    interesting = [token for token in doc if token.pos_ in interesting_pos_set]
+    interesting_words = [token.lemma_ for token in interesting]
 
-# NOTE: Here we use data, which is not cleaned so SPACY can use it to do better POS and NER.
-import time
-start_time = time.time()
-doc_all = nlp(all_file_content)
-duration =  time.time() - start_time
-print(f" Took {int(duration)} seconds to build document of len:{len(all_file_content)}" )
+    GraphWordDistribution(
+        interesting_words,
+        title=f"Distribution of {interesting_pos} on {corpus_path}",
+        skip=0,
+        length=50,
+    )
+
+
+def GraphScratchForCorpus(corpus_path: str, pos: str = "NOUN VERB ADJ ADV"):
+    nlp = get_nlp_model("en_core_web_lg")
+    corpus = LoadCorpus(corpus_path)
+    doc = DocForCorpus(nlp, corpus)
+    GraphPoSForDoc(pos, doc)
+
 
 # %%
-# Remove domain specific stop words.
-doc = [token for token in doc_all if token.text.lower() not in domain_stop_words]
+corpus_paths = ["~/gits/igor2/750words/2018*md", "~/gits/igor2/750words/2019*md"]
+# "/mnt/c/Users/idvor/OneDrive/backup/Diary/*txt"
+
+for c in corpus_paths:
+    GraphScratchForCorpus(c, pos="NOUN")
+
+# %% [markdown]
+# # Debugging when stuff goes goofy.
 
 # %%
+_ = """
 max_to_analyze = 15
 interesting = [token for token in doc if token.tag_ == "NNS"]
 for token in interesting[:max_to_analyze]:
     # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_)
 
-
-# %%
-GraphWordDistribution([token.pos_ for token in doc])
-
-# %%
-# interesting =  [token for token in  doc]
 # Parts of speech: https://spacy.io/usage/linguistic-features
-# interesting_pos = "PROPN"
-interesting_pos = "NOUN"
-interesting_pos_set = set(interesting_pos.split())
-interesting = [token for token in doc if token.pos_ in interesting_pos_set]
+GraphWordDistribution([token.pos_ for token in doc], title=f"POS Distribution on {corpus_path}")
 # interesting = [ token for token in doc if token.pos_ != "PUNCT" and token.pos_ != "SYM" and len(token.text) > 3]
-interesting_words = [token.lemma_ for token in interesting]
-GraphWordDistribution(
-    interesting_words,
-    title=f"Lemmatized parts of speech distribution for: {interesting_pos}",
-    skip=0,
-    length=50,
-)
+"""
 
 # %%
