db611782040cc6c2e5b50b3ad9f6e68a4b6f7ef9
Igor Dvorkin <idvorkin@gmail.com>
2020-12-24 15:34:48 -0800

    Display multiple correlation matrix's

--- py_generated/stock_analysis.py
+++ py_generated/stock_analysis.py
@@ -36,13 +36,12 @@ import pandas_datareader.data as data
 # # Lets take a stab at some stock analysis!
 
 # +
-# We would like all available data from 01/01/2000 until 12/31/2016.
-start_date = "2015-01-01"
+start_date = "2017-06-01"  # SNAP  IPO'd on 3/2/2017 - so start after that.
 end_date = "2020-12-31"
 
 # User pandas_reader.data.DataReader to load the desired data. As simple as that.
 panel_data = data.DataReader(
-    "FB;AMZN;SNAP;AAPL;QQQ;GOOG;MSFT".split(";"), "yahoo", start_date, end_date
+    "FB;AMZN;SNAP;AAPL;QQQ;GOOG;MSFT;GOLD".split(";"), "yahoo", start_date, end_date
 )
 print("Sample from the full dataset")
 display(panel_data.head(1))
@@ -87,36 +86,56 @@ df.pct_change().sum()
 from numpy.core.defchararray import encode
 from altair.vegalite.v4.schema.channels import Tooltip
 
-# Look at correlation
 print("Correlations between stocks")
 print("  NOTE: Need to correlate on percentage change, not abosolute price")
-df = df_original.copy()
-df.columns.name = None
-corr = df.pct_change(1).corr()  # compute correlation on percent change
-display(corr)
-corr = df.corr().reset_index().melt(id_vars="index")
-# display(corr)
-height_in_inches = 40
 
-base = (
-    alt.Chart(corr)
-    .properties(
-        width=8 * height_in_inches,
-        height=8 * height_in_inches,
+
+def display_correlation_matrix(df, earliest, latest):
+    df = df_original.copy()
+    df.columns.name = None
+    corr = df.pct_change(1).corr()  # compute correlation on percent change
+    df_dates = df[earliest:latest]
+    corr = df_dates.corr().reset_index().melt(id_vars="index")
+    # display(corr)
+    height_in_inches = 40
+
+    base = (
+        alt.Chart(corr)
+        .properties(
+            width=8 * height_in_inches,
+            height=8 * height_in_inches,
+            title=f"Stock Correlation from: {earliest} to {latest}",
+        )
+        .encode(
+            x="index:O",
+            y="variable:O",
+        )
     )
-    .encode(
-        x="index:O",
-        y="variable:O",
+
+    c = base.mark_rect().encode(
+        tooltip="value;index;variable".split(";"), color="value:Q"
     )
-)
+    text = base.mark_text(baseline="middle").encode(
+        text=alt.Text("value:Q", format="0.2f")
+    )
+    display(c + text)
 
-c = base.mark_rect().encode(tooltip="value;index;variable".split(";"), color="value:Q")
 
+# Look at correlation
+year_ago = arrow.utcnow().shift(months=-12).date()
+today = arrow.utcnow().date()
+latest = today
+cv_19_start = "2020-02-01"
 
-# Configure text
-text = base.mark_text(baseline="middle").encode(text=alt.Text("value:Q", format="0.2f"))
+import datetime
 
-display(c + text)
+first_day = df_original.reset_index().iloc[0, 0]
+
+display_correlation_matrix(df, year_ago, today)
+display_correlation_matrix(df, first_day, today)
+display_correlation_matrix(df, "2020-06-01", today)
+display_correlation_matrix(df, "2019-01-01", cv_19_start)
+display_correlation_matrix(df, cv_19_start, today)
 
 
 # + active=""
